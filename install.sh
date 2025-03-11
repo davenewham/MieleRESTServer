70c30fac4d37826af71ef4b5af0c1eb86243117b
Alexander Kappner <alexander@kappner.info>
2025-03-10 18:50:36 -0700

    install.sh: further refactored install script to prepare for virtualenv

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 INSTALL_DIR="/usr/local/lib/MieleRESTServer/"
-
+LOCAL_ACCOUNT="mieleserver"
 #!/bin/bash
 systemctl stop MieleRESTServer
 mkdir -p "$INSTALL_DIR"
 cp *.py "$INSTALL_DIR"
 cp -r templates/ "$INSTALL_DIR"
 cp *.service /etc/systemd/system/
-useradd --system mieleserver
-sudo -u mieleserver pip install -r ./requirements.txt
+useradd --system "$LOCAL_ACCOUNT"
+sudo -u "$LOCAL_ACCOUNT" pip install -r ./requirements.txt
 systemctl daemon-reload
 systemctl restart MieleRESTServer
